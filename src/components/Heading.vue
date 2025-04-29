5a6f15a719d43dcf98fda9ad301ea805bf52d221
faithreeves00 <faithreeves@letu.edu>
2025-04-29 16:11:48 -0500

    heading

---
+++ src/components/Heading.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex justify-between items-center text-teal-500">
+    <h1 class="text-4xl font-italianno">The Meadow</h1>
+  <div class="flex text-base font-inter lg:gap-x-12">
+    <text>Home</text> 
+    <text>About Us</text>
+    <text>Contact Us</text>
+    <text>Services</text>
+  </div>
+  <div class="flex lg:gap-6">
+    <img src="../assets/search.svg" alt="search"/>
+    <img src="../assets/account_circle.svg" alt="user"/>
+    <img src="../assets/shopping_cart.svg" alt="cart"/>
+  </div>
+</div>
+</template>
