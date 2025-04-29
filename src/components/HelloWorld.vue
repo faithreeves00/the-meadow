3a83e7e6a0c797c1771f9569a75c84b4dfff8839
faithreeves00 <faithreeves@letu.edu>
2025-04-29 16:11:37 -0500

    hero section

--- src/components/HelloWorld.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-</script>
-
-<template>
-  <h1 class="text-black-500">Hello World</h1>
-</template>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex items-center justify-between lg:pt-15">
+    <img
+      src="../assets/meadow.png"
+      alt="hero"
+      class="max-w-[355px] rounded-xl"
+    />
+    <div class="flex flex-col justify-between max-w-[517px] lg:min-h-[236px]">
+      <div class="flex flex-col text-teal-500 text-left text-wrap lg:gap-y-6">
+        <text class="font-italiana lg:text-5xl">Welcome to The Meadow</text>
+        <text class="font-inter lg:text-xl"
+          >Explore nature's beauty with carefully curated floral designs that
+          bring elegance and tranquility to any space.</text
+        >
+      </div>
+
+      <button
+        class="text-white bg-teal-500 hover:bg-teal-600 rounded-full font-inter lg:text-base lg:w-[210px] lg:h-[45px]"
+      >
+        Explore the Collection
+      </button>
+    </div>
+  </div>
+</template>
