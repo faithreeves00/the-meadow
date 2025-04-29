b189c9ff5eb8674303fd9135c5d886bf4aa29013
faithreeves00 <faithreeves@letu.edu>
2025-04-29 16:11:13 -0500

    config updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        italianno: ["Italianno", "cursive"],
+        italiana: ["Italiana", "sans-serif"],
+      },
+      colors: {
+        cream: {
+          500: "#FCFAF6", // Replace with your desired cream color hex code
+        },
+      },
+    },
   },
   variants: {
     extend: {},
