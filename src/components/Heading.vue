a86c6f0eaf1b38b15d421d252eb805989a6a96a5
faithreeves00 <faithreeves@letu.edu>
2025-04-29 17:43:51 -0500

    header underline on hover

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -5,10 +5,10 @@
   <div class="flex justify-between items-center text-teal-500">
     <h1 class="text-4xl font-italianno">The Meadow</h1>
   <div class="flex text-base font-inter lg:gap-x-12">
-    <text>Home</text> 
-    <text>About Us</text>
-    <text>Contact Us</text>
-    <text>Services</text>
+    <text class="underline underline-offset-5 decoration-1">Home</text> 
+    <text class="hover:underline hover:underline-offset-5 hover:decoration-1">About Us</text>
+    <text class="hover:underline hover:underline-offset-5 hover:decoration-1">Contact Us</text>
+    <text class="hover:underline hover:underline-offset-5 hover:decoration-1">Services</text>
   </div>
   <div class="flex lg:gap-6">
     <img src="../assets/search.svg" alt="search"/>
