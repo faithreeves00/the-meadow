da702df8843f3954fd0963147d7e91d4966b9e19
faithreeves00 <faithreeves@letu.edu>
2025-04-29 17:44:13 -0500

    page updates

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Heading from './components/Heading.vue'
+import HeroSection from './components/HeroSection.vue';
+import GallerySection from './components/GallerySection.vue';
 </script>
 
 <template>
-  <div class="min-h-screen bg-white">
-    <HelloWorld/>
+
+  <div class="bg-neutral-500 lg:px-10 lg:p-6">
+    <Heading/>
+    <HeroSection/>
   </div>
+  <div class="bg-teal-500 lg:px-10 lg:py-16">
+  <GallerySection/>
+  </div>
+
 </template>
