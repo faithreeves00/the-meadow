18ce075f68def77e34c667da637b955a9f9673cf
faithreeves00 <faithreeves@letu.edu>
2025-05-01 17:13:31 -0500

    clean up hero section

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,19 +1,30 @@
 <template>
-  <div class="flex flex-col items-center justify-start reverse items-start py-8 gap-y-8 md:py-15 md:gap-x-17 md:flex-row lg:flex-row lg:py-16 lg:gap-x-17">
+  <div
+    class="flex flex-col items-center justify-start reverse items-start py-8 gap-y-8 md:py-15 md:gap-x-17 md:flex-row lg:flex-row lg:py-16 lg:gap-x-17"
+  >
+    <!-- Hero Image -->
     <img
       src="../assets/images/meadow.png"
-      alt="hero"
+      alt="A meadow with flowers"
       class="max-w-[355px] rounded-xl"
     />
-    <div class="flex flex-col justify-between h-full max-w-[517px] gap-y-7 md:min-h-[236px] lg:min-h-[236px]">
-      <div class="flex flex-col text-teal-500 text-left text-wrap gap-y-4 lg:gap-y-6">
-        <text class="font-italiana text-4xl md:text-4xl lg:text-5xl">Welcome to The Meadow</text>
-        <text class="font-inter text-base md:text-base lg:text-xl"
-          >Explore nature's beauty with carefully curated floral designs that
-          bring elegance and tranquility to any space.</text
-        >
+    <!-- Hero Text -->
+    <div
+      class="flex flex-col justify-between h-full max-w-[517px] gap-y-7 md:min-h-[236px] lg:min-h-[236px]"
+    >
+      <div
+        class="flex flex-col text-teal-500 text-left text-wrap gap-y-4 lg:gap-y-6"
+      >
+        <h1 class="font-italiana text-4xl md:text-4xl lg:text-5xl">
+          Welcome to The Meadow
+        </h1>
+        <p class="font-inter text-base md:text-base lg:text-xl">
+          Explore nature's beauty with carefully curated floral designs that
+          bring elegance and tranquility to any space.
+        </p>
       </div>
 
+      <!-- Hero Button -->
       <button
         class="drop-shadow-lg self-start px-4.5 py-2 text-white bg-teal-500 hover:bg-teal-600 rounded-full font-inter text-base lg:w-[210px] lg:h-[45px]"
       >
