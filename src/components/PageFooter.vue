aabd5d3a8291194616d0a99e1ec62be6a22c2e37
faithreeves00 <faithreeves@letu.edu>
2025-05-01 17:08:22 -0500

    clean up footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="flex justify-center gap-20 md:gap-28 lg:gap-60">
+  <div class="flex justify-center gap-20 md:gap-28 lg:gap-60">
+    <!-- Business Info  -->
     <div class="flex flex-col text-teal-500 sm:gap-y-3">
-        <text class="text-4xl font-italianno">The Meadow</text>
-        <text class="text-inter">501 Market St.<br>Austin, Texas<br>78731</text>
+      <h2 class="text-4xl font-italianno">The Meadow</h2>
+      <p class="text-inter">501 Market St.<br />Austin, Texas<br />78731</p>
     </div>
 
+    <!-- Contact Info -->
     <div class="flex flex-col text-teal-500 text-inter gap-y-4">
-        <text>(123) 456-7899</text>
-        <text>contact@themeadow.com</text>
+      <p>(123) 456-7899</p>
+      <p>contact@themeadow.com</p>
     </div>
 
+    <!-- Links -->
     <div class="flex flex-col text-teal-500 text-inter gap-y-4">
-        <text>About</text>
-        <text>FAQs</text>
+      <a href="#">About</a>
+      <a href="#">FAQs</a>
     </div>
-</div>
+  </div>
 </template>
