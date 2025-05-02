f8816607424dc782ef8acfb22863be09edcfa472
faithreeves00 <faithreeves@letu.edu>
2025-05-02 10:10:34 -0500

    fix icon file paths

--- src/components/header/IconsSection.vue
+++ src/components/header/IconsSection.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex gap-4 lg:gap-6">
-    <img src="../assets/icons/search.svg" alt="search icon" />
-    <img src="../assets/icons/account_circle.svg" alt="user account icon" />
+    <img src="../../assets/icons/search.svg" alt="search icon" />
+    <img src="../../assets/icons/account_circle.svg" alt="user account icon" />
     <img
-      src="../assets/icons/teal_shopping_cart.svg"
+      src="../../assets/icons/teal_shopping_cart.svg"
       alt="shopping cart icon"
     />
   </div>
