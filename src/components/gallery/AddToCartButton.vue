d73114b87e92b24d96e8e50b319aecf9961118a7
faithreeves00 <faithreeves@letu.edu>
2025-05-02 10:28:22 -0500

    update colors for accessability

--- src/components/gallery/AddToCartButton.vue
+++ src/components/gallery/AddToCartButton.vue
@@ -8,13 +8,21 @@ defineProps({
 </script>
 
 <template>
-   <div class="group">
-          <button
-            class="flex border-2 border-teal-500 rounded-full w-[44px] h-[44px] items-center justify-center bg-transparent text-teal-500 hover:bg-teal-500 hover:text-white"
-            aria-label="Add {{ name }} to cart"
-          >
-            <img src="../../assets/icons/teal_shopping_cart.svg" alt="" class="group-hover:hidden" />
-            <img src="../../assets/icons/cream_shopping_cart.svg" alt="" class="hidden group-hover:block" />
-          </button>
-        </div>
+  <div class="group">
+    <button
+      class="flex border-2 border-teal-500 rounded-full w-[44px] h-[44px] items-center justify-center bg-transparent text-teal-500 hover:bg-teal-600 hover:border-teal-600 hover:text-white"
+      aria-label="Add {{ name }} to cart"
+    >
+      <img
+        src="../../assets/icons/teal_shopping_cart.svg"
+        alt="add to cart"
+        class="group-hover:hidden"
+      />
+      <img
+        src="../../assets/icons/cream_shopping_cart.svg"
+        alt="add to cart"
+        class="hidden group-hover:block"
+      />
+    </button>
+  </div>
 </template>
